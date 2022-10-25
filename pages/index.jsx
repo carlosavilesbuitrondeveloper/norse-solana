e84f1e41809f5656b2894d1922cf17037d9ebf66
Carlos Aviles Buitron <carlosavilesbuitron@carloss-mbp.myfiosgateway.com>
2022-10-25 10:38:01 -0400

    Implement factions component with shadow background for contrast

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import Wrapper from '../components/wrapper'
 import Hero from '../components/hero'
 import Meta from '../components/meta'
+import Factions from '../components/factions'
+import ShadowWrapper from '../components/shadowWrapper'
 
 export default function Home() {
 	const factions = [
@@ -13,19 +15,19 @@ export default function Home() {
 		{
 			name: 'Cultists',
 			role: 'Corrupted',
-			imageUrl: '/images/ui-faction-05.jpg',
+			imageUrl: '/images/ui-faction-02.jpg',
 			bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
 		},
 		{
 			name: 'Protectors',
 			role: 'Pure',
-			imageUrl: '/images/ui-faction-06.jpg',
+			imageUrl: '/images/ui-faction-03.jpg',
 			bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
 		},
 		{
 			name: 'Supernaturals',
 			role: 'Ethereal',
-			imageUrl: '/images/ui-faction-08.jpg',
+			imageUrl: '/images/ui-faction-04.jpg',
 			bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
 		},
 	]
@@ -34,7 +36,13 @@ export default function Home() {
 		<>
 			<Meta title='Norse NFT | Home' />
 			<Wrapper>
+				{/* Hero */}
 				<Hero image='https://storage.neptunecs.com/fenrir/ui/wolf.jpg' />
+
+				{/* Factions */}
+				<ShadowWrapper>
+					<Factions title='The Factions' factions={factions} />
+				</ShadowWrapper>
 			</Wrapper>
 		</>
 	)
