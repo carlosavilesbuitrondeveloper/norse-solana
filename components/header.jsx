22db3b7088b3f1c5522c7ec7b061cb23df027ca0
Carlos Aviles Buitron <carlosavilesbuitron@carloss-mbp.myfiosgateway.com>
2022-10-25 10:54:26 -0400

    Implement sidebar into existing header

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,20 @@
 import { HiOutlineMenu } from 'react-icons/hi'
+import { useState } from 'react'
+import SideNav from './sidenav'
 
 export default function Header() {
+	const [open, setOpen] = useState(false)
+
 	return (
 		<div className='decorative w-full h-12 bg-black text-white flex justify-between items-center p-2 relative z-50'>
+			<SideNav collapsed={open} setCollapsed={setOpen} />
 			<div className='flex items-center flex-1'>
-				<button className='text-white text-2xl'>
+				<button
+					className='text-white text-2xl'
+					onClick={() => {
+						setOpen(!open)
+					}}
+				>
 					<HiOutlineMenu />
 				</button>
 				<div className='ml-3 text-xl'>Norse NFT</div>
